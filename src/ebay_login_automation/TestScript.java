380f4fe2a36905c616182ad4fe1713f6e89d22cf
Saroj Senadheera <ooxygen89@gmail.com>
2018-08-22 12:38:10 +0530

    updated_version

--- src/ebay_login_automation/TestScript.java
+++ src/ebay_login_automation/TestScript.java
@@ -1,18 +1,21 @@
 package ebay_login_automation;
 
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class TestScript {
 
     private WebDriver driver;
     private String URL;
-    private String email = "b7058880@nwytg.net";
-    private String password = "abcd@1234";
+    private String user;
 
     @BeforeTest // first thing to run , or you can just set priority to 1 same thing
     public void setup() throws Exception {
@@ -34,17 +37,52 @@ public class TestScript {
         //downside to this will be translated pages	
     }
 
-    @Test(priority = 4)
-    public void signIn() {
+    @Test(priority = 4, dataProvider = "getData")// this is to iterate between different combinations of username and passwords
+    public void signIn(String email, String password) {
         driver.findElement(By.linkText("Sign in")).click();// we select the sign in and click on it
         driver.findElement(By.xpath("//*[@id=\"userid\"]")).sendKeys(email); // we select the user id textfield and enter an email
         driver.findElement(By.xpath("//*[@id=\"pass\"]")).sendKeys(password);// we select the password field and enter the password
         driver.findElement(By.xpath("//*[@id=\"sgnBt\"]")).click();// and we click on sign in button
+
+        if (driver.getCurrentUrl().contains("DisplaySUorNPBPopUp")) {
+            driver.findElement(By.xpath("//*[@id=\"but_submitBtn\"]")).click();	// if account is suspended this is going to trigger
+            user = driver.findElement(By.xpath("//button[@id='gh-ug']/b")).getText();// get button text 
+            assertEquals("User", user);// check if my username matches with my actual username
+            
+        } else {
+            signOut();
+        }
+    }
+
+    @Test(priority = 5)
+    public void signOut() {
+        driver.findElement(By.xpath("/html/body/div[3]/div/header/div/ul[1]/li[1]/button/b[2]")).click();// trying to find the dropdown for the option signout
+
+        try {
+            driver.findElement(By.linkText("Sign out")).click();// clicking on the link sign out will sure throw an exception for noSuchElement
+        } catch (Exception e) {
+            driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);// wait one second until the page correctly renders
+        } finally {
+            driver.findElement(By.partialLinkText("out")).click();// after the rendering clicking on the signout again
+        }
+        assertTrue(driver.getCurrentUrl().contains("SignOutConfirm"));// making sure that we have successfully signed out
+        System.out.println("Sign out Successful");
+
     }
 
     @AfterTest
     public void closeAll() {
         driver.close();
+    }
+
+    @DataProvider
+    public Object[][] getData() {
+        Object[][] credentials = new Object[1][2];
+
+        credentials[0][0] = "b7058880@nwytg.net";
+        credentials[0][1] = "abcd@1234";
+
+        return credentials;
     }
 
 }
